469ea3668d0589e18fd84162ed30c1e2d6496b3f
Elterror <71641771+Elterror@users.noreply.github.com>
2021-06-07 15:29:15 +0200

    Created bindAdapterToListView

--- app/src/main/java/com/example/cartrade/MainActivity.java
+++ app/src/main/java/com/example/cartrade/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.cartrade;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MainAdapter adapter;
+    private ArrayList<Car> carList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        carList = new ArrayList<>();
+    }
+
+
+    public void bindAdapterToListView(ListView listView) {
+        adapter = new MainAdapter(this, R.layout.activity_adapter, carList);
+
+        listView.setAdapter(adapter);
     }
 }
