52b468929da110d6ccb768cc170dd25428513aa5
Elterror <71641771+Elterror@users.noreply.github.com>
2021-06-07 14:45:02 +0200

    Create Class Car
    Created Car-Class + Object variables

---
+++ app/src/main/java/com/example/cartrade/Car.java
@@ -0,0 +1,13 @@
+package com.example.cartrade;
+
+public class Car {
+    private String name;
+    private int price;
+    private String first_registration;
+    private int ps;
+    private int kilometres;
+    private String description;
+    private String location;
+    private String telNumber;
+
+}
