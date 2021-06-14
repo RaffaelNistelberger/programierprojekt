b96000a6eae66b7d363ab0d6c5e0ea5cff32af95
Elterror <71641771+Elterror@users.noreply.github.com>
2021-06-14 13:47:34 +0200

    add new Car Method +xml + new Activity

--- app/src/main/java/com/example/cartrade/MainActivity.java
+++ app/src/main/java/com/example/cartrade/MainActivity.java
@@ -1,32 +1,16 @@
 package com.example.cartrade;
 
-import androidx.annotation.RequiresApi;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-
-import android.os.Message;
-
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-
 import android.widget.LinearLayout;
 import android.widget.ListView;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -36,15 +20,19 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Car> carList;
     private ListView listView;
     private LinearLayout linearLayout;
+    private final int ADD_ACTIVITY_REQUEST_CODE = 187;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //login();
-        //loadData();
-        saveData();
-
+        login();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -60,21 +48,10 @@ public class MainActivity extends AppCompatActivity {
             case R.id.search_bar:
                 //Methode hinzufügen
                 return true;
-            case R.id.priceInc:
-                sortListbyPriceAsc();
-                return true;
-            case R.id.priceDesc:
-                sortListbyPriceDesc();
-                return true;
         }
         return super.onOptionsItemSelected(item);
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+
 
     public void login(){
         carList = new ArrayList<>();
@@ -91,23 +68,6 @@ public class MainActivity extends AppCompatActivity {
         listView.setAdapter(adapter);
     }
 
-    private void loadData(){
-        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot ds : snapshot.getChildren()){
-                    System.out.println(ds.getValue().toString());
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-    }
-
     public void sortListbyPriceDesc(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             carList.sort((o1, o2) -> o2.getPrice() -o1.getPrice());
@@ -121,15 +81,27 @@ public class MainActivity extends AppCompatActivity {
             bindAdapterToListView(listView);
         }
     }
-    private void saveData(){
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("cars");
-
-        myRef.setValue(new CarEntity(1,"Test3"));
-    }
 
     public void add(){
+        Intent intent = new Intent(MainActivity.this, AddActivity.class);
+        startActivityForResult(intent, ADD_ACTIVITY_REQUEST_CODE);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //returns here when AddActivity upload is finished
+        if(requestCode == ADD_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Car tmp = new Car(data.getStringExtra("Name"), Integer.parseInt(data.getStringExtra("Price")), data.getStringExtra("First_registration"), Integer.parseInt(data.getStringExtra("Ps")), Integer.parseInt(data.getStringExtra("Kilometres")), data.getStringExtra("Descripotion"), data.getStringExtra("Location"), data.getStringExtra("TelNumber"));
+                carList.add(tmp);
+                bindAdapterToListView(listView);
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+
+            }
+        }
     }
 
 }
