4f5356ea375fca8995747c46c79613eb9bf9edc2
xxkeppsxx <71641659+xxkeppsxx@users.noreply.github.com>
2021-06-14 12:46:02 +0200

    showPrefs() + preferenceChanged()
    showPrefs() + preferenceChanged() created and implemented

--- app/src/main/java/com/example/cartrade/MainActivity.java
+++ app/src/main/java/com/example/cartrade/MainActivity.java
@@ -2,14 +2,20 @@ package com.example.cartrade;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -19,13 +25,18 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Car> carList;
     private ListView listView;
     private LinearLayout linearLayout;
+    private final static int RQ_PREFERENCES = 1;
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         login();
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -41,7 +52,9 @@ public class MainActivity extends AppCompatActivity {
                 //Methode hinzufügen
                 return true;
             case R.id.settings:
-                //Methode hinzufügen
+                Intent intent = new Intent(this,
+                        MySettingsActivity.class);
+                startActivityForResult(intent, RQ_PREFERENCES);
                 return true;
             case R.id.search_bar:
                 //Methode hinzufügen
@@ -51,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void login(){
+    public void login() {
         carList = new ArrayList<>();
         linearLayout = findViewById(R.id.linearLayout);
         listView = findViewById(R.id.listView);
@@ -64,5 +77,18 @@ public class MainActivity extends AppCompatActivity {
         adapter = new MainAdapter(this, R.layout.activity_adapter, carList);
 
         listView.setAdapter(adapter);
+    }
+
+    public void showPrefs(View view) {
+        String backgroundColor = prefs.getString(getString(R.string.Darkmode_pref), "#FFFFFF");
+        view.setBackgroundColor(Color.parseColor(backgroundColor));
+    }
+
+    private void preferenceChanged(SharedPreferences sharedPrefs, String key) {
+        if (key.equals("Darkmode_pref")) {
+            String sValue = sharedPrefs.getString(key, "");
+            Toast.makeText(this, key + " new Background: " + sValue, Toast.LENGTH_LONG).show();
+            recreate();
+        }
     }
 }
