4bef15ada7c44588878d2e542a53e261f6a3b3fc
Elterror <71641771+Elterror@users.noreply.github.com>
2021-06-14 15:33:16 +0200

    Reiter Elias, Select Image, bitmap toString, Storage Permissions

--- app/src/main/java/com/example/cartrade/AddActivity.java
+++ app/src/main/java/com/example/cartrade/AddActivity.java
@@ -1,19 +1,30 @@
 package com.example.cartrade;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class AddActivity extends AppCompatActivity {
 
@@ -26,6 +37,10 @@ public class AddActivity extends AppCompatActivity {
     private EditText location;
     private EditText telNumber;
     private Button upload;
+    private Button pickPhoto;
+    private final int PICK_PHOTO_FOR_AVATAR = 888;
+    private ImageView carImage;
+    private final int ACCESS_FINE_STORAGE = 123;
 
 
     @Override
@@ -42,6 +57,7 @@ public class AddActivity extends AppCompatActivity {
         telNumber = findViewById(R.id.editText_TelNumber);
         upload = findViewById(R.id.button_Upload);
         location = findViewById(R.id.editText_Location);
+        carImage = findViewById(R.id.carImage);
 
         upload.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,6 +66,63 @@ public class AddActivity extends AppCompatActivity {
             }
         });
 
+        pickPhoto = findViewById(R.id.pickPhoto);
+        pickPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPermissionExternalStorage();
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_PHOTO_FOR_AVATAR && resultCode == Activity.RESULT_OK) {
+            if (data == null) {
+                //Display an error
+                return;
+            }
+            try {
+                InputStream inputStream = this.getContentResolver().openInputStream(data.getData());
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                carImage.setImageBitmap(bitmap);
+                System.out.println(bitMapToString(bitmap));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public String bitMapToString(Bitmap bitmap){
+        ByteArrayOutputStream baos=new  ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100, baos);
+        byte [] b=baos.toByteArray();
+        String temp= Base64.encodeToString(b, Base64.DEFAULT);
+        return temp;
+    }
+
+    private void checkPermissionExternalStorage() {
+        String permission = Manifest.permission.READ_EXTERNAL_STORAGE;
+        String permission2 = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+        if (ActivityCompat.checkSelfPermission(this, permission)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, permission2)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{permission},
+                    ACCESS_FINE_STORAGE);
+        } else {
+            pickImage();
+        }
+    }
+
+    public void pickImage() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_PHOTO_FOR_AVATAR);
     }
 
     public void returnIntent(){
